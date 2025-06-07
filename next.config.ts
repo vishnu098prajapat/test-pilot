be43b45b6c57eb6ac15d64e7395b159a7c9de3b9
Vishnu Prajapati <vishanuprajapati888@gmail.com>
2025-06-07 10:50:54 +0000

    ye run karo     allowedOrigins: ['https://9003-firebase-studio-174918414

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  experimental: {
+    allowedOrigins: ['https://6000-firebase-studio-1749184145527.cluster-zumahodzirciuujpqvsniawo3o.cloudworkstations.dev'],
   },
 };
 
